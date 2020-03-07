685e254388b55a0d6eabb748530e0e0fd6a02316
Marcos Sanz González <msanz@profile.local>
2020-03-07 23:45:32 +0100

    feat: cambios por google

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var bodyParser = require('body-parser');
 // Inicializar variables
 var app = express();
 
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
+app.get('/', function (req, res, next) {
+    // Handle the get for this route
+});
+
+app.post('/', function (req, res, next) {
+    // Handle the post for this route
+});
 
 // Body Parser
 // parse application/x-www-form-urlencoded
